5e11ce482a541505c8e9a47bd6a035f43712c353
TomasBelfonte <tomas.belfonte@gmail.com>
2023-02-07 15:59:29 +0100

    complete

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -51,7 +51,7 @@
             <img src="{{ asset('storage/' . $project->cover_img) }}" alt="" class="img-thumbnail">
             </div>
 
-            <a href="{{ route('admin.project.index') }}" class="btn btn-secondary">Annulla</a>
+            <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Annulla</a>
             <button class="btn btn-primary">Salva</button>
         </form>
 
